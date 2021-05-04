01b79d172297e01d622043fb70a600d7db8ac8c0
mingyuchoo <mingyuchoo@gmail.com>
2021-05-05 06:23:01 +0900

    initial

---
+++ spring-boot-01/src/main/java/com/example/demo/FizzBuzz.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+public class FizzBuzz {
+    public String play(int number) {
+        if (number == 3)
+            return "Fizz";
+        else if (number == 15)
+            return "FizzBuzz";
+        else
+            return "Buzz";
+
+    }
+}
